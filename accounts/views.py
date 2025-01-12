3dbee13f485064fe5357d359803a50e92b780b87
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-13 01:57:29 +0900

    Feat: 회원가입 기능 정의
    - models.py : AbstractUser를 확장한 'CustomUser' 클래스 생성, 기본 필드에 email 추가
    - settings.py에 'CustomUser'를 로그인용 모델로 정의
    - url 선언
    - views.py : serializer 통해 회원가입 진행, 성공 시 메세지와 함께 201 반환; 실패 시 400 반환
    - serializers.py : 필드 선언, create 메소드로 자동생성되는 id 제외 필드 입력 로직 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+
+from accounts.serializers import CustomUserSerializer
 
 # Create your views here.
+
+User = get_user_model()
+
+class RegisterView:
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "회원가입 완료"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
