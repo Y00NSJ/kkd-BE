b1c23caea347cd42f6bf1489b1575392159def37
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-13 02:07:55 +0900

    Feat: 로그아웃 기능 정의
    - views.py : refresh 토큰을 블랙리스트에 추가함으로써 무효화

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts.serializers import CustomUserSerializer
 
@@ -16,3 +17,14 @@ class RegisterView:
             serializer.save()
             return Response({"message": "회원가입 완료"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView:
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "로그아웃 완료"}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
